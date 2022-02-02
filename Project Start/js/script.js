e38c520cca30411193d3e3e5cb01bb41456aa41c
TheLeon <leonrej90@gmail.com>
2022-02-02 22:27:53 +0500

    Build the Tabs

--- Project Start/js/script.js
+++ Project Start/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+    // LOADER
     const loader = document.querySelector(".loader");
     setTimeout(function() {
         loader.style.opacity = "0";
@@ -6,4 +7,40 @@ window.addEventListener("DOMContentLoaded", () => {
             loader.style.display = "none";
         }, 1500);
     }, 2000);
+
+    // TABS
+    const headerParent = document.querySelector(".tabheader__items"),
+        tabs = document.querySelectorAll(".tabheader__item"),
+        tabContents = document.querySelectorAll(".tabcontent");
+
+    function hideTabContent() {
+        tabContents.forEach((item) => {
+            item.style.display = "none";
+        });
+
+        tabs.forEach((item) => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabContents[i].style.display = "block";
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    headerParent.addEventListener("click", (e) => {
+        if (e.target && e.target.classList.contains("tabheader__item")) {
+            console.log(e.target);
+            tabs.forEach((item, i) => {
+                if (e.target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+                // console.log(item, i);
+            });
+        }
+    });
 });
