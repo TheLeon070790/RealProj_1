90dac2bbf0a9c1cc310ec57beeb93c627b4c085d
TheLeon <leonrej90@gmail.com>
2022-02-03 22:28:32 +0500

    Build the Modal

--- Project Start/js/script.js
+++ Project Start/js/script.js
@@ -1,3 +1,5 @@
+// "use strict";
+
 window.addEventListener("DOMContentLoaded", () => {
     // LOADER
     const loader = document.querySelector(".loader");
@@ -15,7 +17,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
     function hideTabContent() {
         tabContents.forEach((item) => {
-            item.style.display = "none";
+            item.classList.add("hide");
+            item.classList.remove("show");
         });
 
         tabs.forEach((item) => {
@@ -24,7 +27,8 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     function showTabContent(i = 0) {
-        tabContents[i].style.display = "block";
+        tabContents[i].classList.remove("hide");
+        tabContents[i].classList.add("show");
         tabs[i].classList.add("tabheader__item_active");
     }
 
@@ -33,7 +37,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     headerParent.addEventListener("click", (e) => {
         if (e.target && e.target.classList.contains("tabheader__item")) {
-            console.log(e.target);
+            // console.log(e.target);
             tabs.forEach((item, i) => {
                 if (e.target == item) {
                     hideTabContent();
@@ -43,4 +47,48 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
     });
+
+    // MODAL
+    const allModalBtn = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector(".modal"),
+        modalClose = document.querySelector("[data-close]");
+
+    allModalBtn.forEach((btn) => {
+        btn.addEventListener("click", openModal);
+    });
+
+    modalClose.addEventListener("click", closeModal);
+
+    function openModal() {
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        clearInterval(timerModel);
+    }
+
+    function closeModal() {
+        modal.classList.add("hide");
+        modal.classList.remove("show");
+        document.body.style.overflow = "";
+    }
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    const timerModel = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (
+            window.pageYOffset + document.documentElement.clientHeight >=
+            document.documentElement.scrollHeight
+        ) {
+            openModal();
+            window.removeEventListener("scroll", showModalByScroll);
+        }
+    }
+
+    window.addEventListener("scroll", showModalByScroll);
 });
