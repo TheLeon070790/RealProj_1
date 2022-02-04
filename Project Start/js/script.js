ca2432c2e56f0ca1d1ce2960c2e1625d51fdeb70
TheLeon <leonrej90@gmail.com>
2022-02-04 11:43:19 +0500

    Build the Date

--- Project Start/js/script.js
+++ Project Start/js/script.js
@@ -91,4 +91,54 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     window.addEventListener("scroll", showModalByScroll);
+
+    // DATE
+    const deadline = "2022-02-06";
+
+    function getTime(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(total / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((total / (1000 * 60)) % 24),
+            seconds = Math.floor((total / 1000) % 24);
+        return {
+            total: total,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return "0" + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = document.querySelector("#days"),
+            hours = document.querySelector("#hours"),
+            minutes = document.querySelector("#minutes"),
+            seconds = document.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const time = getTime(endtime);
+            days.innerHTML = getZero(time.days);
+            hours.innerHTML = getZero(time.hours);
+            minutes.innerHTML = getZero(time.minutes);
+            seconds.innerHTML = getZero(time.seconds);
+            if (time.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock(".timer", deadline);
 });
