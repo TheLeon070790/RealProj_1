6d10289eb6d890811dff5bf880c1f7c3b6226f00
TheLeon <leonrej90@gmail.com>
2022-02-05 11:39:47 +0500

    Build the Slider and buttons

--- Project Start/js/script.js
+++ Project Start/js/script.js
@@ -78,7 +78,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    const timerModel = setTimeout(openModal, 5000);
+    // const timerModel = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
         if (
@@ -219,4 +219,43 @@ window.addEventListener("DOMContentLoaded", () => {
         "399.000",
         ".menu .container"
     ).render();
+
+    // SLIDER
+    const slides = document.querySelectorAll(".offer__slide"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        current = document.querySelector("#current"),
+        total = document.querySelector("#total");
+
+    let slideIndex = 1;
+    show(slideIndex);
+
+    function show(s) {
+        if (s > slides.length) {
+            slideIndex = 1;
+        }
+        if (s < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => (item.style.cssText = "display: none"));
+        slides[slideIndex - 1].style.display = "block";
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function sliderPlus(s) {
+        show((slideIndex += 1));
+    }
+
+    prev.addEventListener("click", () => {
+        sliderPlus(-1);
+    });
+
+    next.addEventListener("click", () => {
+        sliderPlus(1);
+    });
 });
